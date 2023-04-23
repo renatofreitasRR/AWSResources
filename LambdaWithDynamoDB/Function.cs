d98bfc354fc0fca50633c0c255c846227024df32
Renato Freitas <natogfreitas@gmail.com>
2023-04-23 19:46:39 -0300

    add more policy permisions and crud using api gateway

--- LambdaWithDynamoDB/Function.cs
+++ LambdaWithDynamoDB/Function.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -9,19 +11,91 @@ namespace LambdaWithDynamoDB;
 
 public class Function
 {
-    
+    private readonly DynamoDBContext _dynamoDbContext;
+
+    public Function()
+    {
+        _dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+    }
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public async Task<User> FunctionHandler(Guid input, ILambdaContext context)
+    public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
     {
-        var dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
-        var user = await dynamoDbContext.LoadAsync<User>(input);
+        return request.RequestContext.Http.Method.ToUpper() switch
+        {
+            "GET" => await HandleGetRequest(request),
+            "POST" => await HandlePostRequest(request),
+            "DELETE" => await HandleDeleteRequest(request),
+        };
+    }
 
-        return user;
+    private async Task<APIGatewayHttpApiV2ProxyResponse> HandleGetRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
+        request.PathParameters.TryGetValue("userId", out var userIdString);
+        if (Guid.TryParse(userIdString, out var userId))
+        {
+            var user = await _dynamoDbContext.LoadAsync<User>(userId);
+
+            if (user is not null)
+            {
+                return new APIGatewayHttpApiV2ProxyResponse()
+                {
+                    Body = JsonSerializer.Serialize(user),
+                    StatusCode = 200
+                };
+            }
+        }
+
+        return BadResponse("Invalid userId in path");
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse> HandlePostRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
+        var user = JsonSerializer.Deserialize<User>(request.Body);
+
+        if (user == null)
+        {
+            return BadResponse("Invalid user properties");
+        }
+
+        await _dynamoDbContext.SaveAsync(user);
+
+        return OkResponse();
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse> HandleDeleteRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
+        request.PathParameters.TryGetValue("userId", out var userIdString);
+
+        if (Guid.TryParse(userIdString, out var userId))
+        {
+            await _dynamoDbContext.DeleteAsync<User>(userId);
+
+            return OkResponse();
+        }
+
+        return BadResponse("Invalid userId in path");
+    }
+
+    private static APIGatewayHttpApiV2ProxyResponse OkResponse()
+    {
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            StatusCode = 200
+        };
+    }
+
+    private APIGatewayHttpApiV2ProxyResponse BadResponse(string message)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse()
+        {
+            Body = message,
+            StatusCode = 400
+        };
     }
 }
 
