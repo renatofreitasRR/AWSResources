52875ed6661fddc6406f66ae590368703975183a
Renato Freitas <natogfreitas@gmail.com>
2023-04-22 23:07:41 -0300

    add aws lambda with dynamodb

---
+++ LambdaWithDynamoDB/Function.cs
@@ -0,0 +1,32 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace LambdaWithDynamoDB;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async Task<User> FunctionHandler(Guid input, ILambdaContext context)
+    {
+        var dynamoDbContext = new DynamoDBContext(new AmazonDynamoDBClient());
+        var user = await dynamoDbContext.LoadAsync<User>(input);
+
+        return user;
+    }
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
